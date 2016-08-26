3f4280a84e22b4f1bc9b9c1854a38d42b6b87cf7
Carling Sugarman <csugarman@hmc.edu>
2016-08-26 12:26:06 -0700

    [pull] - replaces input with searchable select bar from react-select

--- client/components/LocationSearchBar.js
+++ client/components/LocationSearchBar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import cityNames from '../data/dummyCities';
+import Select from 'react-select';
 
 class LocationSearchBar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            cities: cityNames
+            cities: cityNames,
+            cityChoice: null
         };
     }
 
@@ -13,8 +15,16 @@ class LocationSearchBar extends React.Component {
         return (
             <div>
                 <h1> Where are you? </h1>
-                <input placeholder="Enter your city, state" onChange={(evt) =>
-                    {this.props.handleLocationChange(evt)}}/>
+                <Select
+                    placeholder="Choose a city..."
+                    value={this.state.cityChoice}
+                    options={this.state.cities}
+                    onChange={(choice) => {
+                        console.log(choice.value)
+                        this.setState({cityChoice: choice});
+                        this.props.handleLocationChoice(choice.value)
+                    }}
+                />
                 <button onClick={() => {this.props.handleLocationSearch()}}> Find Food Near Me </button>
             </div>
         );
--- client/data/dummyCities.js
+++ client/data/dummyCities.js
@@ -1,8 +1,32 @@
 const cityNames = [
-    'Los Angeles, CA',
-    'Santa Monica, CA',
-    'Chicago, IL',
-    'New York, NY'
+    {
+        value: {
+            city: 'Los Angeles',
+            state: 'CA'
+        },
+        label: 'Los Angeles, CA'
+    },
+    {
+        value: {
+            city: 'Santa Monica',
+            state: 'CA'
+        },
+        label: 'Santa Monica, CA'
+    },
+    {
+        value: {
+            city: 'Chicago',
+            state: 'IL'
+        },
+        label: 'Chicago, IL'
+    },
+    {
+        value: {
+            city: 'New York',
+            state: 'NY'
+        },
+        label: 'New York, NY'
+    }
 ];
 
 export default cityNames;
