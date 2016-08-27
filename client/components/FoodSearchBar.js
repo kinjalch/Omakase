8b645214ad1479120bf9af8511826055aa48b1ff
Carling Sugarman <csugarman@hmc.edu>
2016-08-27 12:16:36 -0700

    [feature] - adds voteApp component, adds voteSurvey component, adds restaurantSearchBar component, refactors search side to make foodSearchBar and locationSearchBar reusable

--- client/components/FoodSearchBar.js
+++ client/components/FoodSearchBar.js
@@ -13,23 +13,16 @@ class FoodSearchBar extends React.Component {
 
     render() {
         return (
-            <div>
-                <div>
-                    <h1> What are you craving? </h1>
-                    <Select
-                        placeholder="I'm craving..."
-                        clearable={false}
-                        value={this.state.foodChoice}
-                        options={this.state.foodTypes}
-                        onChange={(choice) => {
-                            this.setState({foodChoice: choice});
-                            this.props.handleFoodChoice(choice)
-                        }}
-                    />
-                    <button onClick={() => {this.props.handleFoodSearch()}}> Curate The Best </button>
-                </div>
-                <button onClick={() => {this.props.navigateToLocationSearch()}}> Back </button>
-            </div>
+            <Select
+                placeholder={this.props.foodPlaceholder}
+                clearable={false}
+                value={this.state.foodChoice}
+                options={this.state.foodTypes}
+                onChange={(choice) => {
+                    this.setState({foodChoice: choice});
+                    this.props.handleFoodChoice(choice);
+                }}
+            />
         );
     }
 }
