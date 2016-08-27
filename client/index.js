5c7359a6832672de31f66ef53e8938948849594b
Carling Sugarman <csugarman@hmc.edu>
2016-08-27 12:16:36 -0700

    [feature] - adds voteApp component, adds voteSurvey component, refactors search side to make foodSearchBar and locationSearchBar reusable

--- client/index.js
+++ client/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SearchApp from "./components/SearchApp.js";
+import VoteApp from "./components/VoteApp.js";
 
-ReactDOM.render(<SearchApp />, document.getElementById("app"));
+// ReactDOM.render(<SearchApp />, document.getElementById("app"));
+ReactDOM.render(<VoteApp />, document.getElementById("app"));
