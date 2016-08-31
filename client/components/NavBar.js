7c0bc8a66e3ffb5560a07621bd8c04fdac98363c
Carling Sugarman <csugarman@hmc.edu>
2016-08-31 12:15:34 -0700

    [feature] - adds link and template for about page

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -6,7 +6,8 @@ var NavBar = (props) => (
         <Nav bsStyle="pills" className="button-bar">
             <a href="#/signup"><Button bsStyle="default" className="nav-button"> Sign Up </Button></a>
             <a href="#/login"><Button bsStyle="default" className="nav-button"> Login </Button></a>
-            <a href={props.navLink}><Button bsStyle="default" className="nav-button float-right"> {props.navMessage} </Button></a>
+            <a href={props.navLink}><Button bsStyle="default" className="nav-button float-right pad-right"> {props.navMessage} </Button></a>
+            <a href="#/about"><Button bsStyle="default" className="nav-button float-right"> About </Button></a>
         </Nav>
         <div className="nav-logo">
             <a href="#"><img src='./logo_medium.jpg'/></a>
