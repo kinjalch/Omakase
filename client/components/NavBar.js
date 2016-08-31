d982fde1b6feff9c34a109421a00767c8d96c3de
Carling Sugarman <csugarman@hmc.edu>
2016-08-30 21:34:42 -0700

    [style] - adds arrow back button on food search page

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -3,13 +3,13 @@ import { Nav, NavItem, Button } from 'react-bootstrap';
 
 var NavBar = (props) => (
     <div className="nav-bar">
-        <Nav bsStyle="pills">
+        <Nav bsStyle="pills" className="button-bar">
             <a href="#/signup"><Button bsStyle="default" className="nav-button"> Sign Up </Button></a>
             <a href="#/login"><Button bsStyle="default" className="nav-button"> Login </Button></a>
             <a href="#/vote"><Button bsStyle="default" className="nav-button float-right"> {props.navMessage} </Button></a>
         </Nav>
         <div className="nav-logo">
-        <a href="#"><img src='./logo_medium.jpg'/></a>
+            <a href="#"><img src='./logo_medium.jpg'/></a>
         </div>
     </div>
 );
