1e0bc55925ccd84b170c048e0bd4223c74bfd10b
Carling Sugarman <csugarman@hmc.edu>
2016-09-01 14:56:03 -0700

    [style] - creates smaller nav bar

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -6,12 +6,10 @@ var NavBar = (props) => (
         <Nav bsStyle="pills" className="button-bar">
             <a href="/signup"><Button bsStyle="default" className="nav-button"> Sign Up </Button></a>
             <a href="/login"><Button bsStyle="default" className="nav-button"> Login </Button></a>
+            <a href="/"><img className="logo-center" src='./logo.jpg'/></a>
             <a href={props.navLink}><Button bsStyle="default" className="nav-button float-right pad-right"> {props.navMessage} </Button></a>
             <a href="/about"><Button bsStyle="default" className="nav-button float-right"> About </Button></a>
         </Nav>
-        <div className="nav-logo">
-            <a href="/"><img src='./logo_medium.jpg'/></a>
-        </div>
     </div>
 );
 
