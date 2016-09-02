402df134bf722f9746abcc6c4e41049f1f9982b6
Carling Sugarman <csugarman@hmc.edu>
2016-09-02 11:18:20 -0700

    [setup] - populates dummy data with more food and city options

--- client/data/dummyFoods.js
+++ client/data/dummyFoods.js
@@ -1,6 +1,13 @@
 const foodTypes = [
     {
         value: {
+            foodName: 'Apple Pie',
+            image: './images/applepie.jpg'
+        },
+        label: 'Apple Pie'
+    },
+    {
+        value: {
             foodName: 'Banh Mi',
             image: './images/banhmi.png'
         },
@@ -8,10 +15,136 @@ const foodTypes = [
     },
     {
         value: {
+            foodName: 'Burrito',
+            image: './images/burrito.jpg'
+        },
+        label: 'Burrito'
+    },
+    {
+        value: {
+            foodName: 'Caesar Salad',
+            image: './images/caesarsalad.jpeg'
+        },
+        label: 'Caesar Salad'
+    },
+    {
+        value: {
+            foodName: 'Cheeseburger',
+            image: './images/cheeseburger.jpg'
+        },
+        label: 'Cheeseburger'
+    },
+    {
+        value: {
+            foodName: 'Chicago Style Pizza',
+            image: './images/chicagostylepizza.jpg'
+        },
+        label: 'Chicago Style Pizza'
+    },
+    {
+        value: {
+            foodName: 'Cupcakes',
+            image: './images/cupcakes.jpg'
+        },
+        label: 'Cupcakes'
+    },
+    {
+        value: {
+            foodName: 'Donuts',
+            image: './images/donuts.jpeg'
+        },
+        label: 'Donuts'
+    },
+    {
+        value: {
+            foodName: 'Fettucine Alfredo',
+            image: './images/fettucinealfredo.jpg'
+        },
+        label: 'Fettucine Alfredo'
+    },
+    {
+        value: {
+            foodName: 'Grilled Cheese',
+            image: './images/grilledcheese.jpg'
+        },
+        label: 'Grilled Cheese'
+    },
+    {
+        value: {
+            foodName: 'Hamburger',
+            image: './images/hamburger.jpg'
+        },
+        label: 'Hamburger'
+    },
+    {
+        value: {
+            foodName: 'Hotdog',
+            image: './images/hotdog.jpg'
+        },
+        label: 'Hotdog'
+    },
+    {
+        value: {
+            foodName: 'New York Style Pizza',
+            image: './images/newyorkstylepizza.jpg'
+        },
+        label: 'New York Style Pizza'
+    },
+    {
+        value: {
+            foodName: 'Pasta',
+            image: './images/pasta.jpg'
+        },
+        label: 'Pasta'
+    },
+    {
+        value: {
+            foodName: 'Philly Cheesesteak',
+            image: './images/phillycheesesteak.jpg'
+        },
+        label: 'Philly Cheesesteak'
+    },
+    {
+        value: {
+            foodName: 'Pho',
+            image: './images/pho.jpg'
+        },
+        label: 'Pho'
+    },
+    {
+        value: {
+            foodName: 'Pizza',
+            image: './images/pizza.jpg'
+        },
+        label: 'Pizza'
+    },
+    {
+        value: {
+            foodName: 'Poke',
+            image: './images/poke.jpg'
+        },
+        label: 'Poke'
+    },
+    {
+        value: {
+            foodName: 'Steak',
+            image: './images/steak.jpg'
+        },
+        label: 'Steak'
+    },
+    {
+        value: {
             foodName: 'Tacos',
             image: './images/tacos.jpg'
         },
         label: 'Tacos'
+    },
+    {
+        value: {
+            foodName: 'Tamales',
+            image: './images/tamales.jpg'
+        },
+        label: 'Tamales'
     }
 ];
 
