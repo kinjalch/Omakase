0f137098ced079a5326113e9a76e5019be1ed3a0
Carling Sugarman <csugarman@hmc.edu>
2016-09-01 17:17:51 -0700

    [feature] - modifies restaurant search to return all info about the restaurant

--- client/components/FoodResult.js
+++ client/components/FoodResult.js
@@ -4,7 +4,9 @@ import { Image, Thumbnail, Button } from 'react-bootstrap';
 var FoodResult = (props) => (
     <div className='food-result'>
         <Thumbnail src={props.foodType.value.image}>
-            <h3>{props.result}</h3>
+            <h3>{props.result.restaurant_name}</h3>
+            <div>{props.result.address}</div>
+            <div>{props.result.location_name} {props.result.zipcode}</div>
         </Thumbnail>
     </div>
 );
