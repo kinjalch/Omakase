2708254cf3f592d5b9b8ec1de59b109e6011745a
Carling Sugarman <csugarman@hmc.edu>
2016-09-02 11:18:20 -0700

    [setup] - populates dummy data with more food and city options

--- client/data/dummyCities.js
+++ client/data/dummyCities.js
@@ -1,31 +1,147 @@
 const cityNames = [
     {
         value: {
-            city: 'Chicago',
-            state: 'IL'
+            city: 'Austin, TX'
+        },
+        label: 'Austin, TX'
+    },
+    {
+        value: {
+            city: 'Baltimore, MD'
+        },
+        label: 'Baltimore, MD'
+    },
+    {
+        value: {
+            city: 'Boston, MA'
+        },
+        label: 'Boston, MA'
+    },
+    {
+        value: {
+            city: 'Chicago, IL'
         },
         label: 'Chicago, IL'
     },
     {
         value: {
-            city: 'Los Angeles',
-            state: 'CA'
+            city: 'Columbus, OH'
+        },
+        label: 'Columbus, OH'
+    },
+    {
+        value: {
+            city: 'Dallas, TX'
+        },
+        label: 'Dallas, TX'
+    },
+    {
+        value: {
+            city: 'Denver, CO'
+        },
+        label: 'Denver, CO'
+    },
+    {
+        value: {
+            city: 'Detroit, MI'
+        },
+        label: 'Detroit, MI'
+    },
+    {
+        value: {
+            city: 'Houston, TX'
+        },
+        label: 'Houston, TX'
+    },
+    {
+        value: {
+            city: 'Indianapolis, IN'
+        },
+        label: 'Indianapolis, IN'
+    },
+    {
+        value: {
+            city: 'Jacksonville, FL'
+        },
+        label: 'Jacksonville, FL'
+    },
+    {
+        value: {
+            city: 'Los Angeles, CA'
         },
         label: 'Los Angeles, CA'
     },
     {
         value: {
-            city: 'New York',
-            state: 'NY'
+            city: 'Memphis, TN'
+        },
+        label: 'Memphis, TN'
+    },
+    {
+        value: {
+            city: 'Nashville, TN'
+        },
+        label: 'Nashville, TN'
+    },
+    {
+        value: {
+            city: 'New York, NY'
         },
         label: 'New York, NY'
     },
     {
         value: {
-            city: 'Santa Monica',
-            state: 'CA'
+            city: 'Oklahoma City, OK'
+        },
+        label: 'Oklahoma City, OK'
+    },
+    {
+        value: {
+            city: 'Philadelphia, PA'
+        },
+        label: 'Philadelphia, PA'
+    },
+    {
+        value: {
+            city: 'Portland, OR'
+        },
+        label: 'Portland, OR'
+    },
+    {
+        value: {
+            city: 'San Antonio, TX'
+        },
+        label: 'San Antonio, TX'
+    },
+    {
+        value: {
+            city: 'San Diego, CA'
+        },
+        label: 'San Diego, CA'
+    },
+    {
+        value: {
+            city: 'San Francisco, CA'
+        },
+        label: 'San Francisco, CA'
+    },
+    {
+        value: {
+            city: 'Santa Monica, CA'
         },
         label: 'Santa Monica, CA'
+    },
+    {
+        value: {
+            city: 'Seattle, WA'
+        },
+        label: 'Seattle, WA'
+    },
+    {
+        value: {
+            city: 'Washington, DC'
+        },
+        label: 'Washington, DC'
     }
 ];
 
