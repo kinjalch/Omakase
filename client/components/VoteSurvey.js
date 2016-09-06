9058137ab27e5a47cce07ac2dd31cfdd18d58f61
Carling Sugarman <csugarman@hmc.edu>
2016-09-05 20:02:49 -0700

    [refactor] - changes voteSurvey to a functional component

--- client/components/VoteSurvey.js
+++ client/components/VoteSurvey.js
@@ -5,35 +5,27 @@ import LocationSearchBar from './LocationSearchBar';
 import RestaurantSearchBar from './RestaurantSearchBar';
 import { Button } from 'react-bootstrap';
 
-class VoteSurvey extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        };
-    }
-
-    render() {
-        return (
-            <div className="vote-survey">
-                <p> I had the best </p>
-                <FoodSearchBar
-                    foodPlaceholder={null}
-                    handleFoodChoice={this.props.handleFoodChoice}
-                />
-                <p> in </p>
-                <LocationSearchBar
-                    cityPlaceholder={null}
-                    handleLocationChoice={this.props.handleLocationChoice}
-                />
-                <p> at </p>
-                <RestaurantSearchBar
-                    handleRestaurantQuery={this.props.handleRestaurantQuery}
-                />
-                <Button className="main-button" onClick={() => {this.props.handleGoogleSearch()}}> Search For Restaurants </Button>
-            </div>
-        );
-    }
+const VoteSurvey = (props) => {
+    return (
+        <div className="vote-survey">
+            <p> I had the best </p>
+            <FoodSearchBar
+                foodPlaceholder={null}
+                handleFoodChoice={props.handleFoodChoice}
+            />
+            <p> in </p>
+            <LocationSearchBar
+                cityPlaceholder={null}
+                handleLocationChoice={props.handleLocationChoice}
+            />
+            <p> at </p>
+            <RestaurantSearchBar
+                hasLocationChoice={props.hasLocationChoice}
+                handleRestaurantChoice={props.handleRestaurantChoice}
+            />
+            <Button bsSize="large" className="main-button" onClick={() => {props.handleGoogleSearch()}}> Search For Restaurants </Button>
+        </div>
+    );
 }
 
 export default VoteSurvey;
