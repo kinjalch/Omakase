b4f648897bfeb7c5ef5b9d9e8f80b9d2bf864856
SeanCox <sean.cox@mac.com>
2016-09-08 11:25:20 -0700

    [feature]direction button progress

--- client/components/DirectionsButton.js
+++ client/components/DirectionsButton.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
+var DirectionButton = (props) => (
+  //should redirect you to maps
+  <button onClick={()=>{console.log('Button Clicked')} bsStyle="primary"}>
+    <p>DirectionButton</p>
+  </button>
+);
+
 // props.result.address
 // props.result.zipcode
 
 //http://maps.apple.com/?q=51.507269,-0.127695
 //http://maps.apple.com/?address=
-
 
 //https://developer.apple.com/library/safari/featuredarticles/iPhoneURLScheme_Reference/
 //MapLinks/MapLinks.html#//apple_ref/doc/uid/TP40007899-CH5-SW1
