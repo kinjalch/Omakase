408bd222905148801970f713d5a225316b77d863
SeanCox <sean.cox@mac.com>
2016-09-08 10:43:30 -0700

    [feature]added directionbutton.js and possible links inside it

---
+++ client/components/DirectionsButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+// props.result.address
+// props.result.zipcode
+
+//http://maps.apple.com/?q=51.507269,-0.127695
+//http://maps.apple.com/?address=
+
+
+//https://developer.apple.com/library/safari/featuredarticles/iPhoneURLScheme_Reference/
+//MapLinks/MapLinks.html#//apple_ref/doc/uid/TP40007899-CH5-SW1
