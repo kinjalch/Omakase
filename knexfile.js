14ad80ee4edc2470bf3c69e95b5c5f1e618243ac
Eugene Row <eugene.row@gmail.com>
2016-09-08 15:47:51 -0700

    [feature] photo upload preview working, still needs CSS

--- knexfile.js
+++ knexfile.js
@@ -2,24 +2,22 @@ var path = require('path')
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    client: 'mysql',
-    connection: {
-      host: 'mysql.kanadachi.com', //or 127.0.0;.1
-      user: process.env.db_username,
-      password: process.env.db_password,
-      database: 'omakase',
-      charset: 'utf8'
-    },
-    pool: {
-      min: 0,
-      max: 7
-    }
-  }
+ development: {
+   client: 'postgres',
+   connection: {
+     host: 'sl-us-dal-9-portal.2.dblayer.com', //or 127.0.0;.1
+     user: process.env.db_username,
+     password: process.env.db_password,
+     port: '10860',
+     database: 'omakase',
+     charset: 'utf8'
+   },
+   pool: {
+     min: 0,
+     max: 7
+   }
+ }
 }
-
-
-
 
 // module.exports = {
 
